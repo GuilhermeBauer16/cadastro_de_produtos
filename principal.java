1880305f12fa23f7e13cf81b0bce29ed1a6afe7d
Guilherme <guilhermebauer16@gmail.com>
2023-07-01 19:10:50 -0300

    Adicionando o arquivo do projeto

---
+++ principal.java
@@ -0,0 +1,15 @@
+
+
+public class principal {
+    public static void main(String[] args ){
+        System.out.println("Cadrastro de produtos");
+        System.out.println("[1]Cadrastro de usuario");
+        System.out.println("[2]Login");
+
+        int idade;
+        idade = 37;
+        System.out.println(idade);
+
+    }
+}
+
